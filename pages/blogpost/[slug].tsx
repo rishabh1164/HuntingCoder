cdd7b178f9f56acaec228201aea48b8b83690902
rishabh1164 <rdwivedi@ext.lakeshorelearning.com>
2024-11-15 23:06:39 +0530

    added useEffect in slug

--- pages/blogpost/[slug].tsx
+++ pages/blogpost/[slug].tsx
@@ -1,39 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/Blogpost.module.css";
-// import javascript from "../../blogdata/how-to-learn-javascript.json";
-// import nextjs from "../../blogdata/how-to-learn-nextjs.json";
-// import flask from "../../blogdata/how-to-learn-flask.json";
 
 // Step 1: find the file corresponding to slug
 // Step 2: Populate them inside the page
 const slug = () => {
   const router = useRouter();
-  // console.log('router query: ', router.query);
   const { slug } = router.query;
-  // let title = "";
-  // let content = "";
-  // if (slug === "learn-javascript") {
-  //   title = javascript.title;
-  //   content = javascript.content;
-  // } else if (slug === "learn-nextjs") {
-  //   title = nextjs.title;
-  //   content = nextjs.content;
-  // } else {
-  //   title = flask.title;
-  //   content = flask.content;
-  // }
+  const [blog, setBlog] = useState<any>({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlog(parsed);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>{slug}</h1>
+        <h1>{blog.title}</h1>
         <hr />
         <hr />
-        <div className={styles.blogpost}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-          dolores, laboriosam doloribus eum maiores quibusdam dignissimos
-          tenetur fugiat molestiae tempore soluta. Vel, quis molestias.
-        </div>
+        <div className={styles.blogpost}>{blog.content} </div>
       </main>
     </div>
   );
